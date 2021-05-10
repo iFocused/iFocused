e8f1648f1780d92c88d41cc9fc52301579ba4e08
Maor.Gornik <gornik.maor@gmail.com>
2021-05-10 15:50:29 -0400

    Fixed freeze when changing tabs

--- src/application/controllers/MainController.java
+++ src/application/controllers/MainController.java
@@ -7,44 +7,101 @@ import application.FxmlLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class MainController implements Initializable {
-	
-	@FXML
-	private BorderPane mainPane;
 
-	
+	final int NUM_TABS = 5;
+
+	@FXML
+	private TabPane tabsPane;
+
+	@FXML
+	private BorderPane dashboardPane;
+
+	@FXML
+	private BorderPane blocklistPane;
+
+	@FXML
+	private BorderPane schedulePane;
+
+	@FXML
+	private BorderPane timerPane;
+
+	@FXML
+	private BorderPane statsPane;
+
+	@FXML
+	private BorderPane settingsPane;
+
 	private void setTab(int tabId) {
 		FxmlLoader fxmlLoader = new FxmlLoader(tabId);
-		AnchorPane currPane = (AnchorPane)fxmlLoader.getTab();
-		
-		if(currPane != null) {
-			mainPane.setCenter(currPane);
+		AnchorPane currPane = (AnchorPane) fxmlLoader.getTab();
+
+		if (currPane != null) {
+			switch (tabId) {
+			case 0:
+				dashboardPane.setCenter(currPane);
+				break;
+
+			case 1:
+				blocklistPane.setCenter(currPane);
+				break;
+
+			case 2:
+				schedulePane.setCenter(currPane);
+				break;
+
+			case 3:
+				/* TODO */
+				break;
+
+			case 4:
+				/* TODO */
+				break;
+
+			case 5:
+				/* TODO */
+				break;
+			}
 		}
 	}
-	
+
+	private void populateTabs() {
+		for (int i = 0; i < NUM_TABS; i++) {
+			setTab(i);
+			
+			/* TODO */
+			if(i == 2) {
+				return;
+			}
+		}
+	}
+
+	private void changeView(int tabId) {
+		tabsPane.getSelectionModel().select(tabId);
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO Auto-generated method stub
-		setTab(0);
+		populateTabs();
 	}
-	
 
-    @FXML
-    void onBlockListSelection(ActionEvent event) {
-    	setTab(1);
-    }
+	@FXML
+	void onDashboardSelection(ActionEvent event) {
+		changeView(0);
+	}
 
-    @FXML
-    void onDashboardSelection(ActionEvent event) {
-    	setTab(0);
-    }
-    
-    @FXML
-    void onScheduelSelection(ActionEvent event) {
-    	setTab(2);
-    }
+	@FXML
+	void onBlockListSelection(ActionEvent event) {
+		changeView(1);
+	}
+
+	@FXML
+	void onScheduelSelection(ActionEvent event) {
+		changeView(2);
+	}
 
 }
