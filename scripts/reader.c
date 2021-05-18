b39a96735cc8e571b6566d5ac1f87aaf7f11c238
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2021-05-17 23:46:03 -0400

    added writing

--- scripts/reader.c
+++ scripts/reader.c
@@ -1,28 +1,94 @@
 #include <stdio.h>
+#include <string.h>
 
+char *filename = "test.txt";
+// char *filename = "/etc/hosts";
 
-const char filename[50] = "test.txt";
+// Function source code taken from:  
+// https://www.geeksforgeeks.org/c-program-find-size-file/ 
+long int findSize(char *file_name)
+{
+    // opening the file in read mode
+    FILE* fp = fopen(file_name, "r");
+  
+    // checking if the file exist or not
+    if (fp == NULL) {
+        printf("File Not Found!\n");
+        return -1;
+    }
+  
+    fseek(fp, 0L, SEEK_END);
+  
+    // calculating the size of the file
+    long int res = ftell(fp);
+  
+    // closing the file
+    fclose(fp);
+  
+    return res;
+}
 
-int main() {
+int reader() {
    FILE *fp;
    char c = 'a';
-   fp = fopen(filename, "r");  
-  
-  
-
-  for (int i = 0; i < 19; i++) {
-      while (c != '\n') {
-        fread(&c, sizeof(char) ,1, fp);
+   fp = fopen(filename, "r"); 
+   long int size = findSize(filename);
+  for (int i = 0; i < size; i++) {
+        fread(&c, sizeof(char),1, fp);
         printf("%c", c);
     }
-    c = 'a';
-  }
-  
-  
-  
-  
-  
-  
-    
    fclose(fp);
+
+   return 0;
+}
+
+/* Check if the file contains a url
+* Return 1 if the file contains the url
+* Return 0 if the file doesn't contain the url
+*/
+int checkUrl(char *filename, char *url) {
+    FILE *fp;
+    char temp[512];
+    int line_num = 1;
+
+	if((fp = fopen(filename, "r")) == NULL) {
+		return(-1);
+	}
+
+    while(fgets(temp, 512, fp) != NULL) {
+		if((strstr(temp, url)) != NULL) {
+			printf("A match found on line: %d\n", line_num);
+			printf("\n%s\n", temp);
+			return 1;
+		}
+		line_num++;
+	}
+
+    fclose(fp);
+
+    return 0;
+}
+
+/* Remove a line number from the host file
+* Return 0 on success 
+* Return 1 on failure
+*/ 
+int remove(int lineNumber) {
+    
+}
+
+/* Write a url to a file
+* Return 0 on success
+* Return 1 if an error occurs
+*/
+int write(char *url) {
+    FILE *fp = fopen(filename, "a");
+    fputs(url, fp);
+    fclose(fp);
+    return 0;
+}
+
+int main () {
+    write("youtube.com");
+    return 0;
 }
---
+++ scripts/reader.h
@@ -0,0 +1,6 @@
+
+int remove(int lineNumber);
+int write(char *url);
+int reader();
+/* Find the number of bytes inside a file*/
+long int findSize(char *file_name);
