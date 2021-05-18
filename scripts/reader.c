78f6a6fdcded38aeaa6217fb0495258bfd86bbf7
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2021-05-17 21:58:33 -0400

    basic reading

--- scripts/reader.c
+++ scripts/reader.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+
+const char filename[50] = "test.txt";
+
 int main() {
    FILE *fp;
-   char buf[1000];
-   fp = fopen("test.txt", "r");
-   fscanf(fp, "%[^\n]", buf);
-   printf("Data from the file:\n%s", buf);
+   char c = 'a';
+   fp = fopen(filename, "r");  
+  
+  
 
+  for (int i = 0; i < 19; i++) {
+      while (c != '\n') {
+        fread(&c, sizeof(char) ,1, fp);
+        printf("%c", c);
+    }
+    c = 'a';
+  }
+  
+  
+  
+  
+  
+  
+    
    fclose(fp);
 }
